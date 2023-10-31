baaa3a355ceb9fe9c3756f67ac5b64c500dcacad
djonnynogueira <djonnynogueira@gmail.com>
2023-10-31 00:01:25 -0300

    Testando automação

---
+++ automacao/auto.py
@@ -0,0 +1,15 @@
+import pyautogui as pa
+import time 
+
+pa.PAUSE = 2
+
+pa.press('win')
+pa.write('chrome')
+pa.press('ENTER')
+pa.click(x=493, y=438) # Keystrokes (pressionando de teclas)
+pa.click(x=-1888, y=-93)
+pa.click(x=-754, y=217)
+pa.PAUSE = 4
+pa.click(x=-1008, y=158)
+pa.click(x=-1000, y=144)
+pa.click(x=-1093, y=132)
---
+++ automacao/pytautogui-position.py
@@ -0,0 +1,8 @@
+# Localizando coordenadas do cursor do mouse
+# mouse x and y
+
+import pyautogui as p
+import time 
+
+time.sleep(5)
+print(p.position())
