465353f68d3f5cfb4d83541f7eb81cbfd38d292b
djonnynogueira <djonnynogueira@gmail.com>
2023-10-30 23:19:01 -0300

    Criando instalador .exe

--- install/imc.py
+++ install/imc.py
@@ -1,9 +1,14 @@
+# Cálculo de IMC
+print('::CÁLCULO DE ÍNDICE DE MASSA CORPORAL::\n')
+
 peso = float(input('Digite seu peso: '))
 altura = float(input('Digite sua altura: '))
+
 imc = peso / (altura * altura)
-print('='*25)
+
+print('='*20)
 print(f'Seu IMC é de {imc:.2f}.')
-print('='*25)
+print('='*20)
 input('Pressione qualquer telcla para sair...')
 
 # Instalando o pyinstaller:         pip install pyinstaller
